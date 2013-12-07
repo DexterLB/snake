8bd207c3265884334d4cf0ff1618f9c17fce60a0
Angel Angelov <hextwoa@gmail.com>
2013-12-07 15:09:57 +0200

    added canvas class

---
+++ canvas.h
@@ -0,0 +1,19 @@
+#ifndef CANVAS_H
+#define CANVAS_H
+
+#include <QWidget>
+
+class Canvas : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Canvas(QWidget *parent = 0);
+
+signals:
+
+public slots:
+    void paintEvent(QPaintEvent *event);
+
+};
+
+#endif // CANVAS_H
